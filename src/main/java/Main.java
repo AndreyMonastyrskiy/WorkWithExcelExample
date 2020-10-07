de8528d77de501ba3a56828d8f8caf6fef318174
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-10-07 11:50:54 +0300

    Read file with legal representative of the child to hashmap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,47 @@
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
         final String patch = "D:\\Projects\\Java\\PFR\\ZakonnyePredstavitely\\";
         try {
             XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(patch + "Законные представители.xlsx"));
+            HashMap<String, Predstavitel> zakonPredstavitel = new HashMap<>();
             XSSFSheet zeroSheet = workbook.getSheetAt(0);
+            //Iterate through each rows one by one
+            Iterator<Row> rowIterator = zeroSheet.iterator();
+            int counter = 1;
+            while (rowIterator.hasNext())
+            {
+                Row row = rowIterator.next();
+                if (counter < 9) {
+                    counter++;
+                    continue;
+                }
+                zakonPredstavitel.put(row.getCell(13).getStringCellValue(),
+                        new Predstavitel(row.getCell(1).getStringCellValue(), row.getCell(3).getStringCellValue()));
+
+                counter++;
+            }
+            System.out.println("Total rows read: " + counter);
+            if (zakonPredstavitel.containsKey("164-457-766 00")) {
+                System.out.println(zakonPredstavitel.get("164-457-766 00").fio + " " +
+                        zakonPredstavitel.get("164-457-766 00").snils);
+            } else {
+                System.out.println("Snils not found!");
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/Predstavitel.java
@@ -0,0 +1,9 @@
+public class Predstavitel {
+    String fio = "";
+    String snils = "";
+
+    public Predstavitel (String fio, String snils) {
+        this.fio = fio;
+        this.snils = snils;
+    }
+}
