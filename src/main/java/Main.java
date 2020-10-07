99e95712d69e7306920b18d2d668389e9cd6cf6b
Andrew Monastyrskiy <a.monastyrskiy@yandex.ru>
2020-10-07 12:39:56 +0300

    Add cells to row (snil and fio) and save file Smolensk1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
@@ -7,6 +11,7 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -34,13 +39,42 @@ public class Main {
 
                 counter++;
             }
+            workbook.close();
             System.out.println("Total rows read: " + counter);
-            if (zakonPredstavitel.containsKey("164-457-766 00")) {
+           /* if (zakonPredstavitel.containsKey("164-457-766 00")) {
                 System.out.println(zakonPredstavitel.get("164-457-766 00").fio + " " +
                         zakonPredstavitel.get("164-457-766 00").snils);
             } else {
                 System.out.println("Snils not found!");
+            }*/
+            System.out.println("Work with мамки_1_ребенок.xls");
+            HSSFWorkbook smol1 = new HSSFWorkbook(new FileInputStream(patch + "Смоленск\\мамки_1_ребенок.xls"));
+            HSSFSheet sml1Sheet = smol1.getSheetAt(0);
+            counter = 0;
+            rowIterator = sml1Sheet.iterator();
+            while (rowIterator.hasNext())
+            {
+                Row row = rowIterator.next();
+                if (counter < 1) {
+                    Cell fioCell = row.createCell(11, CellType.STRING);
+                    Cell snilsCell = row.createCell(12, CellType.STRING);
+                    fioCell.setCellValue("ФИО законного представителя");
+                    snilsCell.setCellValue("СНИЛС законного представителя");
+                    counter++;
+                    continue;
+                }
+                if (zakonPredstavitel.containsKey(row.getCell(4).getStringCellValue())) {
+                    Cell fioCell = row.createCell(11, CellType.STRING);
+                    Cell snilsCell = row.createCell(12, CellType.STRING);
+                    Predstavitel predstavitel = zakonPredstavitel.get(row.getCell(4).getStringCellValue());
+                    fioCell.setCellValue(predstavitel.fio);
+                    snilsCell.setCellValue(predstavitel.snils);
+                }
+                counter++;
             }
+            System.out.println("Total rows read: " + counter);
+            smol1.write(new FileOutputStream(patch + "out\\Smolensk\\мамки_1_ребенок.xls"));
+            System.out.println("Completed");
 
         } catch (IOException e) {
             e.printStackTrace();
